f431e2bc4b2c1fbf724084c02b1e380e88632ebf
Misha Scholte <mishascholte@gmail.com>
2026-01-29 20:04:07 +0100

    feat: Self-host Monoton font and remove Google Fonts CDN

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
+import '@fontsource/monoton/400.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
